1a5f5a11d41f2bb452f88324ccbb19a8a445337b
marjellyween <marjolijn.knops@gmail.com>
2022-11-27 11:02:06 +0100

    created colors constant

---
+++ src/styling/colors.constant.ts
@@ -0,0 +1,31 @@
+export const baseColors = {
+  white: "#ffffff",
+  black: "#000000",
+  grey: "#eff0f1",
+  midGrey: "#f7f7f9",
+  textGrey: "#ACB2C1",
+  statRed: "#1bd0d9",
+  statGreen: "#70c18f",
+};
+
+export const typeColors: { [key: string]: string } = {
+  water: "#7934da",
+  fire: "#7934da",
+  grass: "#7934da",
+  electric: "#b6a42f",
+  ground: "#7e5b27",
+  rock: "#d5d5d4",
+  fairy: "#cd90bf",
+  poison: "#ba7ec7",
+  bug: "#f8d5a3",
+  dragon: "#cd90bf",
+  psychic: "#a8b349",
+  flying: "#93b1b2",
+  fighting: "#a7896d",
+  normal: "#a2acae",
+};
+
+export const gradientColors: { [key: string]: string } = {
+  favorites: "linear-gradient(109.73deg, #65cb9a 0%, #15d0dc 100%)",
+  team: "linear-gradient(109.73deg, #46469c 0%, #7e32e0 100%)",
+};
