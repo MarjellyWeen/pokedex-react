546fdb973089c8bb384a75863846e03edbd86440
marjellyween <marjolijn.knops@gmail.com>
2022-11-27 21:49:36 +0100

    set header in team and favorites screen

--- src/components/screens/favorites.screen.tsx
+++ src/components/screens/favorites.screen.tsx
@@ -1,8 +1,10 @@
 import React, { FC } from "react";
+import { Header } from "../elements/header.element";
 
 export const Favorites: FC = () => {
   return (
-    <div className="App">
+    <div className="App" id="app">
+      <Header />
       <header className="App-header">
         <h1>Favorites</h1>
       </header>
