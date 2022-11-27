b02537e04434fe3714d80ce268e7e87608ea09dd
marjellyween <marjolijn.knops@gmail.com>
2022-11-27 21:46:15 +0100

    added div for popover portal

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { Main } from "./components/main";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <div id="popover"></div>
     <Main />
   </React.StrictMode>
 );
