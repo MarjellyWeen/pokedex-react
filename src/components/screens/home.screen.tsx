a089d951c5157e04812b94974c152140b3b03d4f
marjellyween <marjolijn.knops@gmail.com>
2022-11-27 19:17:12 +0100

    set search input and pokemon list in home

--- src/components/screens/home.screen.tsx
+++ src/components/screens/home.screen.tsx
@@ -1,22 +1,31 @@
 import React, { FC } from "react";
 import { style } from "typestyle";
+import { useAllPokemon } from "../../state/hooks/useAllPokemon";
 import { Header } from "../elements/header.element";
-import { InputElement } from "../elements/input.element";
 import { NavButton } from "../elements/nav-button.element";
+import { PokemonCard } from "../elements/pokemon-card.element";
+import { SearchInput } from "../templates/search-input.template";
 
 export const Home: FC = () => {
   return (
     <div className="App" id="app">
       <Header isListView />
-      <InputElement
+      <SearchInput
+        onChangeTimeoutMilliseconds={500}
         attributes={{
           name: "search",
           placeholder: "Pokemon zoeken",
+          // defaultValue: searchQuery,
+          // onChange: (event) => setSearchQuery(event.target.value),
         }}
+        iconName="magnifying-glass"
       />
       <div className={styles.buttonContainer}>
         <NavButton key="team" /> <NavButton isFavorites key="favorites" />
       </div>
+      {useAllPokemon().pokemonList.map((pokemon) => (
+        <PokemonCard key={pokemon.name} pokemon={pokemon} />
+      ))}
     </div>
   );
 };
