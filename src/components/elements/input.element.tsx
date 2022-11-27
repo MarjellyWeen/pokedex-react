2b26281f7f295d5c58f6e05a4c1fb51935c06370
marjellyween <marjolijn.knops@gmail.com>
2022-11-27 11:02:45 +0100

    created custom input element

---
+++ src/components/elements/input.element.tsx
@@ -0,0 +1,33 @@
+import React, { InputHTMLAttributes } from "react";
+import { classes, style } from "typestyle";
+import { baseColors } from "../../styling/colors.constant";
+
+type Props = {
+  attributes?: InputHTMLAttributes<HTMLInputElement>;
+};
+
+export function InputElement(props: Props): JSX.Element {
+  return (
+    <input
+      {...{
+        ...props.attributes,
+        className: classes(styles.input, props.attributes?.className),
+      }}
+    />
+  );
+}
+
+const styles = {
+  input: style({
+    display: "flex",
+    alignItems: "center",
+    padding: "7px 8px",
+    gap: 6,
+    width: "100%",
+    height: 36,
+    borderRadius: 8,
+    outline: "none",
+    borderWidth: 0,
+    background: baseColors.grey,
+  }),
+};
