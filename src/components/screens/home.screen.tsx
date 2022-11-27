7b895ed00cabf76749576f2a9986cfb17cb3e399
marjellyween <marjolijn.knops@gmail.com>
2022-11-27 13:17:03 +0100

    added nav buttons to home screen

--- src/components/screens/home.screen.tsx
+++ src/components/screens/home.screen.tsx
@@ -1,17 +1,31 @@
 import React, { FC } from "react";
+import { style } from "typestyle";
 import { Header } from "../elements/header.element";
 import { InputElement } from "../elements/input.element";
+import { NavButton } from "../elements/nav-button.element";
 
 export const Home: FC = () => {
   return (
-    <div className="App">
-      <Header />
+    <div className="App" id="app">
+      <Header isListView />
       <InputElement
         attributes={{
           name: "search",
           placeholder: "Pokemon zoeken",
         }}
       />
+      <div className={styles.buttonContainer}>
+        <NavButton key="team" /> <NavButton isFavorites key="favorites" />
+      </div>
     </div>
   );
+};
+
+const styles = {
+  buttonContainer: style({
+    display: "flex",
+    gap: 9,
+    marginTop: 19,
+    marginBottom: 20,
+  }),
 };
