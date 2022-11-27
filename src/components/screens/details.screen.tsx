486d1fa59b3c3df50a0af3aa25281a5afb571511
marjellyween <marjolijn.knops@gmail.com>
2022-11-27 21:49:10 +0100

    get details information and set image

--- src/components/screens/details.screen.tsx
+++ src/components/screens/details.screen.tsx
@@ -1,11 +1,31 @@
 import React, { FC } from "react";
+import { useParams } from "react-router";
+import { usePokemon } from "../../state/hooks/usePokemon";
+import { typeColors } from "../../styling/colors.constant";
+import { DetailImages } from "../elements/detail-images.template";
+import { Header } from "../elements/header.element";
 
 export const Details: FC = () => {
+  const params = useParams<{ name: string }>();
+
+  if (!params.name) return null;
+
+  const result = usePokemon(params.name);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Details</h1>
-      </header>
+    <div
+      className="App"
+      id="app"
+      style={{
+        background: typeColors[result.pokemon?.types[0].type.name ?? "normal"],
+      }}
+    >
+      <Header
+        title={result.pokemon?.name}
+        isLigthTheme
+        onFavoriteClick={() => console.log("fav!")}
+      />
+      <DetailImages sprites={result.pokemon?.sprites} />
     </div>
   );
 };
