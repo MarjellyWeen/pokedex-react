ccd1724d078c7fcd35501f706216d79bf7658f18
marjellyween <marjolijn.knops@gmail.com>
2022-11-28 17:48:01 +0100

    added lightbox and some images

--- src/components/elements/detail-images.element.tsx
+++ src/components/elements/detail-images.element.tsx
@@ -1,18 +1,38 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { style } from "typestyle";
 import { baseColors } from "../../styling/colors.constant";
 import { Icon } from "./icon.element";
 import { ISprites } from "../../state/interfaces/sprites.interface";
+import FsLightbox from "fslightbox-react";
 
 interface IProps {
   sprites?: ISprites;
 }
 
 export const DetailImages: FC<IProps> = ({ sprites }) => {
+  const [toggler, setToggler] = useState(false);
+  // an arry with the images
+
+  if (!sprites) return null;
+
+  const images = [
+    sprites.other["official-artwork"].front_default,
+    sprites.front_default,
+    sprites.back_default,
+    sprites.front_shiny,
+    sprites.back_shiny,
+  ];
+
   return (
     <div className={styles.imageContainer}>
       <Icon className={styles.icon} icon="chevron-left" />
-      <img className={styles.image} src={sprites?.front_default} />
+      <img
+        className={styles.image}
+        onClick={() => setToggler(!toggler)}
+        src={sprites.other["official-artwork"].front_default}
+      />
+
+      <FsLightbox toggler={toggler} sources={images} />
 
       <Icon className={styles.icon} icon="chevron-right" />
     </div>
