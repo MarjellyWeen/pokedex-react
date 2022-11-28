85940b28c4edaa30fe3b5cdba30c2cd3acd2eec7
marjellyween <marjolijn.knops@gmail.com>
2022-11-28 14:41:25 +0100

    set header titles and margin top

--- src/components/screens/favorites.screen.tsx
+++ src/components/screens/favorites.screen.tsx
@@ -1,13 +1,19 @@
 import React, { FC } from "react";
+import { style } from "typestyle";
+import { spacing } from "../../styling/spacing.constant";
 import { Header } from "../elements/header.element";
 
 export const Favorites: FC = () => {
   return (
     <div className="App" id="app">
-      <Header />
-      <header className="App-header">
-        <h1>Favorites</h1>
-      </header>
+      <Header title="Favorites" />
+      <div className={styles.container}></div>
     </div>
   );
+};
+
+const styles = {
+  container: style({
+    paddingTop: spacing.headerSpace,
+  }),
 };
--- src/components/screens/team.screen.tsx
+++ src/components/screens/team.screen.tsx
@@ -1,10 +1,19 @@
 import React, { FC } from "react";
+import { style } from "typestyle";
+import { spacing } from "../../styling/spacing.constant";
 import { Header } from "../elements/header.element";
 
 export const Team: FC = () => {
   return (
     <div className="App" id="app">
-      <Header />
+      <Header title="Team" />
+      <div className={styles.container}></div>
     </div>
   );
+};
+
+const styles = {
+  container: style({
+    paddingTop: spacing.headerSpace,
+  }),
 };
