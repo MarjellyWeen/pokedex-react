272db5eb7db6b71e2cdd37634eb43417e4e0156f
marjellyween <marjolijn.knops@gmail.com>
2022-11-29 22:26:57 +0100

    created handleclick callback for team add

--- src/components/screens/details.screen.tsx
+++ src/components/screens/details.screen.tsx
@@ -1,12 +1,13 @@
 import React, { FC, useCallback, useMemo } from "react";
 import { useNavigate, useParams } from "react-router";
 import { style } from "typestyle";
-import { useDeleteFavoritePokemon } from "../../state/hooks/useDeleteFavoritePokemon";
-import { useGetFavorites } from "../../state/hooks/useGetFavorites";
+import { useDeleteSavedPokemon } from "../../state/hooks/useDeleteSavedPokemon";
+import { useGetSavedPokemon } from "../../state/hooks/useGetSavedPokemon";
 import { useGetPokemon } from "../../state/hooks/useGetPokemon";
-import { usePostFavoritePokemon } from "../../state/hooks/usePostFavoritePokemon";
+import { usePostPokemon } from "../../state/hooks/usePostPokemon";
 import { gradientsTypeColors } from "../../styling/colors.constant";
 import { spacing } from "../../styling/spacing.constant";
+import { ButtonElement } from "../elements/button.element";
 import { DetailImages } from "../elements/detail-images.element";
 import { Header } from "../elements/header.element";
 import { AboutCard } from "../templates/about-card.template copy";
@@ -20,11 +21,14 @@ export const Details: FC = () => {
   if (!params.name) return null;
 
   const result = useGetPokemon(params.name);
-  const { favorites, refetch } = useGetFavorites();
+  const { savedPokemon: favorites, refetch: getFavorites } =
+    useGetSavedPokemon("favorites");
+  const { call: createFavorite } = usePostPokemon("favorites");
+  const { call: removeFavorite } = useDeleteSavedPokemon("favorites");
 
-  // A function to post a pokemon to the favorites list
-  const { call: create } = usePostFavoritePokemon();
-  const { call: remove } = useDeleteFavoritePokemon();
+  const { savedPokemon: team, refetch: getTeam } = useGetSavedPokemon("team");
+  const { call: addToTeam } = usePostPokemon("team");
+  const { call: removeFromTeam } = useDeleteSavedPokemon("team");
 
   // A useCallback function to post a pokemon to the favorites list.
   // This function checks if the pokemon is already in the favorites list before posting.
@@ -35,21 +39,46 @@ export const Details: FC = () => {
     if (
       favorites?.find((favorite) => favorite.name === result?.pokemon?.name)
     ) {
-      remove(result.pokemon.id);
-      refetch();
+      removeFavorite(result.pokemon.id);
+      getFavorites();
     } else if (
       favorites?.find((favorite) => favorite.name === result?.pokemon?.name) ===
       undefined
     ) {
-      create(
+      createFavorite(
         result.pokemon.name,
         result.pokemon.id,
         result.pokemon.types,
         result.pokemon.sprites
       );
-      refetch();
+      getFavorites();
     }
-  }, [result.pokemon, favorites, create, remove, params.name]);
+  }, [result.pokemon, favorites, createFavorite, removeFavorite, params.name]);
+
+  // TODO clean this up, it could possibly be combined with the above function, handleFavoritesClick.
+  const handleAddToTeamClick = useCallback(() => {
+    if (!result.pokemon) {
+      return;
+    }
+    if (team?.length === 6) {
+      return;
+    }
+    if (team?.find((teamItem) => teamItem.name === result?.pokemon?.name)) {
+      removeFromTeam(result.pokemon.id);
+      getTeam();
+    } else if (
+      team?.find((teamItem) => teamItem.name === result?.pokemon?.name) ===
+      undefined
+    ) {
+      addToTeam(
+        result.pokemon.name,
+        result.pokemon.id,
+        result.pokemon.types,
+        result.pokemon.sprites
+      );
+      getTeam();
+    }
+  }, [result.pokemon, team, addToTeam, removeFromTeam, params.name]);
 
   const isFavorite = useMemo(() => {
     if (
@@ -59,7 +88,7 @@ export const Details: FC = () => {
     } else {
       return false;
     }
-  }, [refetch]);
+  }, [getFavorites]);
 
   return (
     <div
@@ -90,6 +119,10 @@ export const Details: FC = () => {
           <EvolutionCard pokemonNumber={result.pokemon?.id} />
         )} */}
       </div>
+      <ButtonElement
+        text="Add to team"
+        attributes={{ onClick: handleAddToTeamClick }}
+      />
     </div>
   );
 };
