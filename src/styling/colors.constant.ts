29f6c654119d6df6126a4691cd7496562f9cbe93
marjellyween <marjolijn.knops@gmail.com>
2022-11-29 14:26:46 +0100

    added type gradients for detail screen

--- src/styling/colors.constant.ts
+++ src/styling/colors.constant.ts
@@ -33,3 +33,33 @@ export const gradientColors: { [key: string]: string } = {
   favorites: "linear-gradient(109.73deg, #65cb9a 0%, #15d0dc 100%)",
   team: "linear-gradient(109.73deg, #46469c 0%, #7e32e0 100%)",
 };
+
+export const gradientsTypeColors: { [key: string]: string } = {
+  water:
+    "linear-gradient(90deg, rgba(116,171,205,1) 0%, rgba(124,209,188,1) 100%)",
+  fire: "linear-gradient(90deg, rgba(236,159,107,1) 0%, rgba(226,182,135,1) 100%)",
+  grass:
+    "linear-gradient(90deg, rgba(185,227,117,1) 0%, rgba(220,235,98,1) 100%)",
+  electric:
+    "linear-gradient(90deg, rgba(221,196,33,1) 0%, rgba(236,230,115,1) 100%)",
+  ground:
+    "linear-gradient(90deg, rgba(157,122,71,1) 0%, rgba(191,171,151,1) 100%)",
+  rock: "linear-gradient(90deg, rgba(162,162,157,1) 0%, rgba(208,224,227,1) 100%)",
+  fairy:
+    "linear-gradient(90deg, rgba(251,202,237,1) 0%, rgba(251,189,163,1) 100%)",
+  poison:
+    "linear-gradient(90deg, rgba(218,155,233,1) 0%, rgba(173,139,228,1) 100%)",
+  bug: "linear-gradient(90deg, rgba(238,144,103,1) 0%, rgba(191,163,137,1) 100%)",
+  dragon:
+    "linear-gradient(90deg, rgba(232,173,219,1) 0%, rgba(161,124,219,1) 100%)",
+  psychic:
+    "linear-gradient(90deg, rgba(213,226,103,1) 0%, rgba(232,186,143,1) 100%)",
+  flying:
+    "linear-gradient(90deg, rgba(182,219,221,1) 0%, rgba(219,227,228,1) 100%)",
+  fighting:
+    "linear-gradient(90deg, rgba(167,137,109,1) 0%, rgba(162,172,174,1) 100%)",
+  normal:
+    "linear-gradient(90deg, rgba(208,226,230,1) 0%, rgba(238,220,203,1) 100%)",
+  ghost:
+    "linear-gradient(90deg, rgba(151,124,194,1) 0%, rgba(242,212,184,1) 100%)",
+};
