aa3b1279dc07c4ef9d2ce2ed376c07654b841b3c
marjellyween <marjolijn.knops@gmail.com>
2022-11-29 22:25:48 +0100

    defined list in team view

--- src/components/screens/team.screen.tsx
+++ src/components/screens/team.screen.tsx
@@ -1,19 +1,47 @@
 import React, { FC } from "react";
+import { useNavigate } from "react-router";
 import { style } from "typestyle";
+import { useGetSavedPokemon } from "../../state/hooks/useGetSavedPokemon";
+import { gradientColors } from "../../styling/colors.constant";
 import { spacing } from "../../styling/spacing.constant";
 import { Header } from "../elements/header.element";
+import { PokemonCard } from "../elements/pokemon-card.element";
 
 export const Team: FC = () => {
+  const navigate = useNavigate();
+  const { savedPokemon: team } = useGetSavedPokemon("team");
+
   return (
-    <div className="App" id="app">
-      <Header title="Team" />
-      <div className={styles.container}></div>
+    <div
+      className="App"
+      id="app"
+      style={{
+        background: gradientColors.team,
+      }}
+    >
+      <Header
+        title="Team"
+        themeColor={gradientColors.team}
+        onBackClick={() => navigate(-1)}
+        isLigthTheme
+      />
+      <div className={styles.container}>
+        {team?.map(({ name, id, sprites, types }) => (
+          <PokemonCard
+            key={name}
+            name={name}
+            id={id}
+            types={types}
+            sprites={sprites}
+          />
+        ))}
+      </div>
     </div>
   );
 };
 
 const styles = {
   container: style({
-    paddingTop: spacing.headerSpace,
+    paddingTop: spacing.headerSpace + spacing.baseSpacing,
   }),
 };
